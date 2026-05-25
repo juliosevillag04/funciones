cee05ceb2caedbc23cdbf047ad93899713b12721
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-25 10:41:45 -0600

    ejercicio 6 terminado

--- funciones/Program.cs
+++ funciones/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+//6. Validar edades utilizando funciones.
+//Validar si es mayor o menor de edad
+Console.Clear();
+bool mayoredad(int edad)
+{
+    return edad >= 18;
+}
+
+void mostrar_resultado(int edad)
+{
+    if (mayoredad(edad))
+    {
+        Console.ForegroundColor=ConsoleColor.Green;
+        Console.WriteLine("Eres mayor de edad");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("Eres menor de edad");
+        Console.ResetColor();
+    }
+}
+
+int edad;
+
+Console.Write("Ingresa tu edad: ");
+edad = int.Parse(Console.ReadLine()!);
+
+mostrar_resultado(edad);
