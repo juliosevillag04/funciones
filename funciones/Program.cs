9afb9da84307bb65d5745084f622b5f7b42d6cbc
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-26 06:59:13 -0600

    feat: ejercicio 7 terminado

--- funciones/Program.cs
+++ funciones/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+//7. Utilizar Math.Pow para calcular potencias. Usando funciones.
+Console.Clear();
+
+//Función
+double potencia(double base_num, double exponente)
+{
+    return Math.Pow(base_num, exponente);
+}
+
+//Validación
+double validar(string mensaje)
+{
+    double numero;
+    while (true)
+    {
+        Console.Write(mensaje);
+        if (double.TryParse(Console.ReadLine(), out numero))
+        {
+            return numero;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: ingrese solo números válidos.");
+            Console.ResetColor();
+        }
+    }
+}
+
+//Ingreso de datos
+double base_num = validar("Número base: ");
+double exponente = validar("Exponente: ");
+
+//Cálculos
+double resultado = potencia(base_num, exponente);
+
+//Mostrar resultado
+Console.WriteLine($"{base_num}^{exponente} = {resultado}");
