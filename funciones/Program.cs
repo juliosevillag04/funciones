cd462961bfe7e4b46aaef5451a4ae67cb23baa70
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-26 07:19:04 -0600

    feat: ejercicio 8 terminado

--- funciones/Program.cs
+++ funciones/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+//8. Convertir cadenas a mayúsculas y minúsculas. Usando funciones
+Console.Clear();
+//Función
+string cadena(string mayusculas, string minusculas)
+{
+    return "Texto en mayúsculas: " + mayusculas.ToUpper() + "\n" + "Texto en minuscúlas: " + minusculas.ToLower();
+}
+//Ingreso de datos
+Console.Write("Ingresa una cadena de texto: ");
+string texto = Console.ReadLine()!;
+//Mostrar resultado
+Console.WriteLine(cadena(texto, texto));
